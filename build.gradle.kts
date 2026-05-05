ec4875e34362948d8c12796b58a481dec4703c51
Nils Dammenhayn <nils.dammenhayn@europace.de>
2026-05-05 10:31:12 +0200

    enforce bouncycastle version (#392)

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ val dependencyVersionsByGroup = mapOf(
     "net.bytebuddy" to libs.versions.byteBuddy.get(),
     "net.java.dev.jna" to libs.netJavaDev.get().version,
     "org.apache.groovy" to libs.groovy.get().version,
+    "org.bouncycastle" to libs.bouncycastle.get().version,
     "org.jetbrains.kotlin" to libs.versions.kotlin.get(),
     "org.junit.jupiter" to libs.versions.junit.get(),
     "org.junit.platform" to libs.versions.junit.get(),
